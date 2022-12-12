eff1e5189fc1b9a963c01cfeb8aaa31fdd5efb74
Trong Nguyen <ngoctrong29@gmail.com>
2022-12-12 15:23:57 -0500

    update hero.js

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -61,7 +61,7 @@ const Hero = ({
         <div className={innerClasses}>
           <div className="hero-content">
             <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
-              Landing template for <span className="text-color-primary">startups</span>
+              Augmented reality visualization <span className="text-color-primary"> for ultrasound guided procedures</span>
             </h1>
             <div className="container-xs">
               <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
