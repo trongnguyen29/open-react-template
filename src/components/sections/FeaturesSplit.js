3405ba33175a129d607b0407259974a03e73ab4a
Trong Nguyen <ngoctrong29@gmail.com>
2022-12-16 11:02:18 -0500

    te

--- src/components/sections/FeaturesSplit.js
+++ src/components/sections/FeaturesSplit.js
@@ -113,8 +113,7 @@ const FeaturesSplit = ({
 
             <div className="split-item">
               <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
-                <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
-                  </div>
+            
                 <h3 className="mt-0 mb-12">
                   No head turning
                   </h3>
